3762cb142e15cfbd33497d416300b3cd586ad9a2
pedro-both <82621491+pedro-both@users.noreply.github.com>
2022-02-08 09:32:16 -0300

    Update Resultado.ts

--- src/app/models/Resultado.ts
+++ src/app/models/Resultado.ts
@@ -5,7 +5,7 @@ import Round from './Round';
 import Objetivo from './Objetivo';
 @Entity('tb_resultado')
 export default class Resultado {
-    @PrimaryColumn('int')
+    /*@PrimaryColumn('int')
     id: number;
 
     @ManyToOne(type => Round)//, { primary: true })
@@ -14,7 +14,13 @@ export default class Resultado {
 
     @ManyToOne(type => Objetivo)//, { primary: true })
     @JoinColumn({ name: "objetivo_id", referencedColumnName: "id" })
-    objetivo_id: Objetivo;
+    objetivo_id: Objetivo;*/
+    
+    @PrimaryColumn('int')
+    round_id: number;
+
+    @PrimaryColumn('int')
+    objetivo_id: number;
     
     @Column({
         type: "enum",
